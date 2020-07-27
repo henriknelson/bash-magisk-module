f2a5b0262a567bd47d966cad7b795eb2eae4235c
Henrik Nelson <henrik@cliffords.nu>
2020-07-27 16:02:26 +0200

    Updated bash to 5.0.18

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ REPLACE="
 print_modname() {
 ui_print "*********************************************"
 ui_print "     bash for Android       	    	       "
-ui_print "         - v5.0.17                            "
+ui_print "         - v5.0.18                           "
 ui_print "         - built by nelshh@xda-developers    "
 ui_print "*********************************************"
 }
